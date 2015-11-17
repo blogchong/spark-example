f948d25c013cc1989fd624c74ae1d26b5c279298
blogchong <blogchong@163.com>
2015-11-17 11:32:06 +0800

    ALS算法实例

--- README.md
+++ README.md
@@ -23,5 +23,37 @@
 
 ###1.3 执行代码
 * 执行命令`./spark-submit --class com.blogchong.spark.mllib.Kmeans  --master spark://192.168.5.200:7077  --num-executors 2 --driver-memory 124m --executor-memory 124m --total-executor-cores 2  /root/spark/hcy/spark-example.jar`<br>
->//需要注意的是，在设置core数以及内存时，最好先参考一下spark-master-id:8080页面中的worker参数，别超过了就行。<br>
+//需要注意的是，在设置core数以及内存时，最好先参考一下spark-master-id:8080页面中的worker参数，别超过了就行。<br>
+* 跑完了，直接到输出文件夹下，找到代码的输出结果即可。<br>
+
+##2 协同推荐ALS算法实例
+###2.1 数据准备
+**用户评分数据，格式: 用户ID,电影ID,评分**<br>
+>1,1,5.0<br>
+ 1,2,1.0<br>
+ 1,3,5.0<br>
+ 1,4,1.0<br>
+ 2,1,5.0<br>
+ 2,2,1.0<br>
+ 2,3,5.0<br>
+ 2,4,1.0<br>
+ 3,1,1.0<br>
+ 3,2,5.0<br>
+ 3,3,1.0<br>
+ 3,4,5.0<br>
+ 4,1,1.0<br>
+ 4,2,5.0<br>
+ 4,3,1.0<br>
+ 4,4,5.0<br>
+
+上传到hdfs的/spark/mllib/data/als路径中。<br>
+
+###2.2 代码打包
+* 在Intellij中，点击file->选择project structure->选择Artifact->添加jar->把乱七八糟的依赖移除->勾选Build on make。<br>
+* 点击Build->选择Build Artifact->选择ReBuild，然后在之前填写的路径下找到jar。<br>
+* 上传到spark中。<br>
+
+###1.3 执行代码
+* 执行命令`./spark-submit --class com.blogchong.spark.mllib.AlsArithmetic  --master spark://192.168.5.200:7077  --num-executors 2 --driver-memory 124m --executor-memory 124m --total-executor-cores 2  /root/spark/hcy/spark-example.jar`<br>
+//需要注意的是，在设置core数以及内存时，最好先参考一下spark-master-id:8080页面中的worker参数，别超过了就行。<br>
 * 跑完了，直接到输出文件夹下，找到代码的输出结果即可。<br>
---
+++ src/main/java/com/blogchong/spark/mllib/AlsArithmetic.scala
@@ -0,0 +1,70 @@
+package com.blogchong.spark.mllib
+
+import org.apache.log4j.{Level, Logger}
+import org.apache.spark.{SparkConf, SparkContext}
+import org.apache.spark.mllib.recommendation.{MatrixFactorizationModel, ALS, Rating}
+
+/**
+ * Author:  blogchong
+ * Blog:    www.blogchong.com
+ * Mailbox: blogchong@163.com
+ * Data:    2015/10/30
+ * Describe:协同过滤中，基于模型的协同，最小二乘法ALS算法
+ */
+object AlsArithmetic {
+  def main(args: Array[String]) {
+
+    // 屏蔽不必要的日志显示在终端上
+    Logger.getLogger("org.apache.spark").setLevel(Level.WARN)
+    Logger.getLogger("org.eclipse.jetty.server").setLevel(Level.OFF)
+
+    // 设置运行环境
+    val conf = new SparkConf().setAppName("ALS")
+    val sc = new SparkContext(conf)
+
+    // 装载数据集，这是一个三列数据，用户ID：电影ID：用户对该电影的评分
+    val data = sc.textFile("hdfs://192.168.5.200:9000/spark/mllib/data/als/test.data")
+    //进行数组化操作
+    val ratings = data.map(_.split(",") match { case Array(user, item, rate) =>
+      Rating(user.toInt, item.toInt, rate.toDouble)
+    })
+
+    //进行ALS三个重要参数设置
+    val rank = 10
+    val numIterations = 10
+    val lambda = 0.1
+
+    // 模型文件训练
+    val model = ALS.train(ratings, rank, numIterations, lambda)
+
+    //评估模型
+    val usersProducts = ratings.map{ case Rating(user, product, rate) =>
+      (user, product)
+    }
+
+    //通过模型RDD进行效果预测
+    val predictions =  model.predict(usersProducts).map {
+      case Rating(user, product, rate) =>
+        ((user, product), rate)
+    }
+
+    val ratesAndPreds = ratings.map { case Rating(user, product, rate) =>
+      ((user, product), rate)
+    }.join(predictions)
+
+    //效果评判，通过计算预测评分的均方误差来衡量
+    val MSE = ratesAndPreds.map { case ((user, product), (r1, r2)) =>
+      val err = (r1 - r2)
+      err * err
+    }.mean()
+    println("Mean Squared Error = " + MSE)
+
+    //保存模型文件
+    val modelPath = "hdfs://192.168.5.200:9000/spark/mllib/result/als"
+    model.save(sc, modelPath)
+    //使用已经生成的模型文件模型文件
+    //val sameModel = MatrixFactorizationModel.load(sc, modelPath)
+
+      sc.stop()
+  }
+}
